bec73f1e2a0ea448d81b67e95f6b7c114a2dc0c3
Charly Joezer <charlyjoezer30@gmail.com>
2024-01-22 07:02:15 +0800

    Menambahkan template sidebar dashboard

--- resources/views/Dashboard/templates/index.blade.php
+++ resources/views/Dashboard/templates/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="/css/dashboard/{{ $css }}">
 </head>
 <body>
@@ -13,9 +14,45 @@
         <img class="logo" src="/assets/image/Lofinz_transparent.png" alt="Lofinz">
         <div class="text">Dashboard Administrator</div>
       </div>
+      <div class="admin_profile">
+        <div class="profile">
+          <img src="/assets/image/profil_default.jpg" alt="">
+          <div class="username_and_role">
+              <div class="username">{{ auth()->guard('administrator')->user()->username }}</div>
+              <div class="role">Admin</div>
+          </div>
+        </div>
+        <a href="" class="logout-button" title="Logout">
+          Logout
+        </a>
+      </div>
+
+
       <ul class="list_link">
         <li class="link_item">
+          <div class="list_link_header">
+              <i class='bx bxs-home'></i>
+              <span>Dashboard</span>
+          </div>
           <a href="">Beranda</a>
+        </li>
+
+        <li class="link_item">
+          <div class="list_link_header">
+              <i class='bx bxs-inbox'></i>
+              <span>Pesanan</span>
+          </div>
+          <a href="">Riwayat Pesanan</a>
+          <a href="">Ulasan Pembeli</a>
+        </li>
+        
+        <li class="link_item">
+          <div class="list_link_header">
+              <i class='bx bxs-box' ></i>
+              <span>Produk</span>
+          </div>
+          <a href="">Tambah Produk</a>
+          <a href="">Edit Produk</a>
         </li>
       </ul>
     </div>
